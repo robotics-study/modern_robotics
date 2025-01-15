96cef8a70d51178ae266afcd1f559fdb4116512b
ladianchad <qhrejddlvltm@gmail.com>
2025-01-16 01:44:33 +0900

    fix layout
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

---
+++ document/src/pages/chapters/Chapter3.tsx
@@ -0,0 +1,17 @@
+import {IChapterData} from "../../../types/global";
+
+const Chapter3 = () => {
+    return (
+        <div>
+            the explanation will be displayed here.
+        </div>
+    )
+}
+export default {
+    title: "Rigid-Body Motions",
+    chapter: 3,
+    contents: Chapter3,
+    supportedExample: {
+        python: false
+    }
+} as IChapterData
