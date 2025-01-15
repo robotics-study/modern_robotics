20114bb0723146ab49e4ef125f51eafa7755478a
ladianchad <qhrejddlvltm@gmail.com>
2025-01-16 01:27:08 +0900

    fix layout
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- document/src/components/HomeChapterListItem.tsx
+++ document/src/components/HomeChapterListItem.tsx
@@ -2,15 +2,17 @@ import {IChapterData} from "../../types/global";
 import React, {useCallback} from "react";
 
 interface ExampleLinkProps {
+    chapter: number
     language: string
 }
 
 const ExampleLinkProps = ({
+                              chapter,
                               language
                           }: ExampleLinkProps) => {
     return <label className="px-2 text-sm border rounded-full py-0.5 shadow">
         <a target="_blank"
-           href={`https://github.com/robotics-study/modern_robotics/tree/main/${language}`}>{
+           href={`https://github.com/robotics-study/modern_robotics/tree/main/sample_code/chapter${chapter}/${language}`}>{
             language
         } code</a>
     </label>
@@ -45,7 +47,7 @@ const HomeChapterListItem = ({
             supportedExample ? <div className="flex justify-end pt-1.5">
                 {
                     Object.keys(supportedExample).map((language, index) => {
-                        return <ExampleLinkProps key={index} language={language}></ExampleLinkProps>
+                        return <ExampleLinkProps key={index} language={language} chapter={chapter}></ExampleLinkProps>
                     })
                 }
             </div> : null
