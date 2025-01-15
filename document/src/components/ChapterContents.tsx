0283d4290ce1456e0c763c9430a8a24dec76e7a6
ladianchad <qhrejddlvltm@gmail.com>
2025-01-16 02:33:36 +0900

    fix style
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- document/src/components/ChapterContents.tsx
+++ document/src/components/ChapterContents.tsx
@@ -10,8 +10,8 @@ const ChapterContents = ({
                          }: ChapterContentsProps) => {
     return <div>
         <h2 className="w-full py-2 px-2 flex justify-between items-center bg-gray-700 text-gray-300 h-8 tracking-wider">
-            <span className="text-sm">Chapter.{chapter}</span>
-            <span className="text-base font-semibold">{title}</span>
+            <span className="text-sm break-keep">Chapter.{chapter}</span>
+            <span className="text-base font-semibold flex flex-wrap justify-end">{title}</span>
         </h2>
         <div className="p-2">
             <Contents/>
