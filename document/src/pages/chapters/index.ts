ce9e771f5c944b1e3daeabcfa45435001afb0a9e
ladianchad <qhrejddlvltm@gmail.com>
2025-01-16 02:25:40 +0900

    added CH3
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- document/src/pages/chapters/index.ts
+++ document/src/pages/chapters/index.ts
@@ -1,10 +1,12 @@
 import {IChapterData} from "../../../types/global";
 import * as CH1 from './Chapter1'
 import * as CH2 from './Chapter2'
+import * as CH3 from './Chapter3'
 
 const data: { readonly default: IChapterData }[] = [
     CH1,
-    CH2
+    CH2,
+    CH3
 ]
 export default data.sort((a, b) => {
     return a.default.chapter - b.default.chapter
