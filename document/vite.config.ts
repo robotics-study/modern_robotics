dd1fe79c892d3dde25b867146f04701477cb5e4c
ladianchad <qhrejddlvltm@gmail.com>
2025-01-18 02:34:59 +0900

    fix
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- document/vite.config.ts
+++ document/vite.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
     return {
-        base: mode === 'production' ? '/modern_robotics' : '/',
+        base: mode == 'production' ? '/modern_robotics' : '/',
         server: {
             host: true,
             port: 3000
