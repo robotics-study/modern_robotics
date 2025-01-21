a58bb635feeedee0f07d02337bbb5969c3e0ec79
ladianchad <qhrejddlvltm@gmail.com>
2025-01-21 16:39:03 +0900

    changed href
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- document/src/components/Header.tsx
+++ document/src/components/Header.tsx
@@ -3,19 +3,23 @@ import {useSearchParams} from "react-router-dom";
 
 interface HeaderProps {
 }
-const Header = ({
-                }: HeaderProps) => {
+
+const Header = ({}: HeaderProps) => {
 
     const [searchParams, setSearchParams] = useSearchParams()
     const callback = useCallback(() => {
-        searchParams.delete('chapter')
-        setSearchParams(searchParams)
+        if (searchParams.get("chapter")) {
+            searchParams.delete('chapter')
+            setSearchParams(searchParams)
+        } else {
+            window.location.href = "/"
+        }
     }, [])
 
     return <h1
         className="w-full text-2xl font-bold py-2 flex items-center justify-center bg-gray-700 text-gray-100 h-10 sm:tracking-widest">
         <label onClick={callback}>
-            <button>Modern Robotics Study</button>
+            <button>{searchParams.get("chapter") ? "Modern Robotics Study" : "Robotics study"}</button>
         </label>
     </h1>
 }
