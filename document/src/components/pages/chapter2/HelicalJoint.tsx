5a06803c0fbde56e6a29bee13cb6b7726f433a67
ladianchad <qhrejddlvltm@gmail.com>
2025-01-21 19:59:08 +0900

    added skew cylinder
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- document/src/components/pages/chapter2/HelicalJoint.tsx
+++ document/src/components/pages/chapter2/HelicalJoint.tsx
@@ -1,16 +1,94 @@
 import Physics3DCanvas from "../../3d/Physics3DCanvas";
-import {Animation, Color3, IAnimationKey, Mesh, Vector3} from "@babylonjs/core";
+import {Animation, Color3, IAnimationKey, Mesh, StandardMaterial, Vector3, VertexData} from "@babylonjs/core";
 import {useCallback} from "react";
 import {useScene} from "react-babylonjs";
 
 
 const Skew = () => {
-
+    const scene = useScene()
     return <cylinder
-        name="skew"
+        name="screw"
         height={15}
         diameter={0.7}
         tessellation={64}
+        onCreated={(instance) => {
+            const customMesh = new Mesh("screw_spine", instance.getScene());
+
+            //Set arrays for positions and indices
+            const positions = [];
+            const indices = [];
+
+            const v = new Vector3();
+            const radius1 = 0.35;
+            const radius2 = 0.45;
+            const count = 15 / 0.0025;
+            const heightDelta = 0.0025;
+            for (let i = 0; i < count; i++) {
+                let ng = i * 0.1;
+                v.x = Math.cos(ng) * radius1;
+                v.y = 15 / 2 - i * heightDelta;
+                v.z = Math.sin(ng) * radius1;
+                positions.push(v.x, v.y, v.z);
+
+                v.x = Math.cos(ng) * radius2;
+                v.y = 15 / 2 - i * heightDelta;
+                v.z = Math.sin(ng) * radius2;
+                positions.push(v.x, v.y, v.z);
+            }
+
+            for (let i = 0; i < count - 1; i++) {
+                indices.push(i * 2, i * 2 + 1, i * 2 + 2);
+                indices.push(i * 2 + 1, i * 2 + 3, i * 2 + 2);
+            }
+
+            //Empty array to contain calculated values
+            const normals = [];
+
+            const vertexData = new VertexData();
+            VertexData.ComputeNormals(positions, indices, normals);
+
+            //Assign positions, indices and normals to vertexData
+            vertexData.positions = positions;
+            vertexData.indices = indices;
+            vertexData.normals = normals;
+            vertexData.applyToMesh(customMesh)
+
+            const material = new StandardMaterial("screw_spine_mat", instance.getScene())
+            material.diffuseColor = new Color3(0.7, 0.3, 0.3)
+            material.backFaceCulling = false
+            customMesh.material = material
+
+            instance.addChild(customMesh)
+
+            const animation = new Animation(
+                "rotation",
+                "rotation.y",
+                30,
+                Animation.ANIMATIONTYPE_FLOAT,
+                Animation.ANIMATIONLOOPMODE_CYCLE
+            )
+            const keys: IAnimationKey[] = [
+                {
+                    frame: 0,
+                    value: instance.position.y
+                }, {
+                    frame: 60,
+                    value: instance.position.y + Math.PI  * 10
+                }, {
+                    frame: 120,
+                    value: instance.position.y
+                }, {
+                    frame: 180,
+                    value: instance.position.y - Math.PI * 10
+                }, {
+                    frame: 240,
+                    value: instance.position.y
+                }
+            ]
+            animation.setKeys(keys)
+            instance.animations = [animation]
+            scene?.beginAnimation(instance, 0, 240, true)
+        }}
     >
         <standardMaterial
             name="skew_mat"
